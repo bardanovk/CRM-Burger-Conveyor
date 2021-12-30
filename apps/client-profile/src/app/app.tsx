0e486565ae18b3c35eaf2a2769cf622f06d5e4c7
bardanovk <bardanovk@gmail.com>
2021-12-30 13:49:18 +0300

    first commit

---
+++ apps/client-profile/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.scss';
+
+
+import { Route, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <div>hello world
+    </div>
+  );
+}
+
+export default App;
